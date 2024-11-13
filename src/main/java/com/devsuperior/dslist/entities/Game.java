da23e0f5537c671fc6d6a18b16d8751088a8cdc4
Everton Rocha <imevertonrch@gmail.com>
2024-11-12 23:50:09 -0300

    game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -23,9 +23,14 @@ public class Game {
     @Column(name = "game_year") // palavra reservada no banco
     private Integer year;
     private String genre;
-    private String platform;
-    private String imageUrl;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 }
